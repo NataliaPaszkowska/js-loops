26c91f2ecaba6c2baddaae61916d5d83cf730932
Natalia Paszkowska <npaszkowska@gmail.com>
2017-06-10 14:06:07 +0200

    commit

--- js/loops.js
+++ js/loops.js
@@ -1,1 +1,75 @@
 'use strict'
+
+/*for ( var i=0; i<4; i++) {
+    console.log(i);
+}*/
+
+/*for ( var i=4; i>=0; i--) {
+    console.log(i);
+}*/
+
+/*var it = 0;
+while (it<5) {
+	console.log(it);
+	it++;
+}*/
+
+/*var iter = 20;
+do {
+	console.log(iter);
+	iter++;
+} while (iter<10);*/
+
+/*var a = 0;
+while ( a < 5 ) {
+	console.log(a++);
+	
+	if ( a == 3 ) {
+		break;}
+	} */
+
+/* postinkrementacja vs preinkremetacje */
+
+/*for (var i=1 ; i<4; ){
+	console.log(++i);}*/
+	/* 
+	i = i + 1
+	console.log(i);
+	 */
+ 
+/*console.log('-------');
+for ( var i=1 ; i<4 ; ) {
+	console.log(i++); }*/
+	/*
+	console.log(i);
+	i = i + 1;
+	*/
+
+for ( var b = 0; b < 6 ; ++b ) {
+	if ( b == 3 ) {
+		continue;
+	} else {
+		
+		console.log(b);
+	}
+	console.log("--");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
